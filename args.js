ca1683e000dd605fa0ac7739ea6f7b377fc8be08
Techoshi <risi@eminent-it.com>
2022-03-29 16:59:01 -0400

    Using Env

--- args.js
+++ args.js
@@ -1,7 +1,7 @@
 module.exports = [
     '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', 
     '0x70997970C51812dc3A010C7d01b50e0d17dc79C8', 
-    '0xf5e3D593FC734b267b313240A0FcE8E0edEBD69a',
+    '0x46113F474051DfE816aB70E6B16bbF66F215f094',
     'https://public-pre-ipfs.s3.amazonaws.com/MissingToothNFT/images/',
     'https://public-pre-ipfs.s3.amazonaws.com/MissingToothNFT/assets/reveal.json'
 ];
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,7 +10,7 @@ async function main() {
     const token = await Token.deploy(
     '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', 
     '0x70997970C51812dc3A010C7d01b50e0d17dc79C8', 
-    '0xf5e3D593FC734b267b313240A0FcE8E0edEBD69a',
+    '0x46113F474051DfE816aB70E6B16bbF66F215f094',
     'https://public-pre-ipfs.s3.amazonaws.com/MissingToothNFT/images/',
     'https://public-pre-ipfs.s3.amazonaws.com/MissingToothNFT/assets/reveal.json');
 
